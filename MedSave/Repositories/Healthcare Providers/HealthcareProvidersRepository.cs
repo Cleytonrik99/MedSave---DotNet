c9b60cfe77e911118da6072140542464998f4549
Cleytonrik99 <enriquecleyton@gmail.com>
2026-04-02 15:47:39 -0300

    Healthcare Providers Repository Implemented

---
+++ MedSave/Repositories/Healthcare Providers/HealthcareProvidersRepository.cs
@@ -0,0 +1,45 @@
+﻿using MedSave.Context;
+using MedSave.Model;
+using MedSave.Repositories.Healthcare_Providers.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedSave.Repositories.Healthcare_Providers;
+
+public class HealthcareProvidersRepository : IHealthcareProvidersRepository
+{
+    private readonly MedSaveContext _context;
+
+    public async Task<HealthcareProviders?> GetByIdAsync(long id)
+    {
+        var search = await _context.HealthcareProviders.FindAsync(id);
+
+        return search;
+    }
+
+    public async Task<IEnumerable<HealthcareProviders>> GetAllAsync()
+    {
+        var search = await _context.HealthcareProviders.ToListAsync();
+
+        return search;
+    }
+
+    public async Task AddAsync(HealthcareProviders healthcareProviders)
+    {
+        _context.HealthcareProviders.AddAsync(healthcareProviders);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(HealthcareProviders healthcareProviders)
+    {
+        _context.HealthcareProviders.Update(healthcareProviders);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(long id)
+    {
+        var search = await _context.HealthcareProviders.FindAsync(id);
+
+        _context.HealthcareProviders.Remove(search);
+        await _context.SaveChangesAsync();
+    }
+}
---
+++ MedSave/Repositories/Healthcare Providers/Interfaces/IHealthcareProvidersRepository.cs
@@ -0,0 +1,12 @@
+﻿using MedSave.Model;
+
+namespace MedSave.Repositories.Healthcare_Providers.Interfaces;
+
+public interface IHealthcareProvidersRepository
+{
+    Task<HealthcareProviders?> GetByIdAsync(long id);
+    Task<IEnumerable<HealthcareProviders>> GetAllAsync();
+    Task AddAsync(HealthcareProviders healthcareProviders);
+    Task UpdateAsync(HealthcareProviders healthcareProviders);
+    Task DeleteAsync(long id);
+}
